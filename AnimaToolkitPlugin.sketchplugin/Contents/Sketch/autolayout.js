4a7c84ea0c78f703f5385b32059be8a344f3231e
edenpulse <edenpulse@gmail.com>
2019-08-23 15:02:50 +0200

    updates + added Anima

---
+++ AnimaToolkitPlugin.sketchplugin/Contents/Sketch/autolayout.js
@@ -0,0 +1,44 @@
+// plugin.js
+// Copyright (c) 2016 Anima App LTD
+//
+
+@import "lib/runtime.js"
+
+function togglePanel(context) {
+    if (NSClassFromString("APSketch") == null) {
+        runtime.loadBundle("AutoLayoutPlugin.bundle");
+        [APSketch setPluginContextDictionary:context];
+    }
+    try {
+        [APSketch setPluginContextDictionary:context];
+    } catch (e) {}
+    
+    try {
+        [APSketch togglePanel];
+    } catch (e) {}
+}
+
+function start(context) {
+    try {
+        runtime.loadBundle("AutoLayoutPlugin.bundle");
+        [APSketch setPluginContextDictionary:context];
+    } catch (e) {
+        try {
+            runtime.loadBundle("AnimaToolkitTests.bundle");
+            [APSketch setPluginContextDictionary:context];
+        } catch (e) {
+        }
+    }
+    try {
+        [APSketch setPluginContextDictionary:context];
+    } catch (e) {}
+    try {
+        [APSketch start];
+    } catch (e) {}
+}
+
+function selectionChanged(context) {
+    try {
+        [[APSketch shared] selectionChanged];
+    } catch (e) {}
+}
